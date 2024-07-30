7ed29f1849477a797aaa980bbf037abfca766f8e
yonasghiwot <yonihabeshaw@gmail.com>
2024-07-30 08:47:41 -0700

    state.py

--- models/state.py
+++ models/state.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import os
+from os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 from models.base_model import BaseModel, Base
 from models.city import City
 
-
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    name = Column(String(128), nullable=False)
+
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship(
-            'City',
-            cascade='all, delete, delete-orphan',
-            backref='state'
-        )
+        cities = relationship('City', cascade='all, delete, delete-orphan', backref='state')
+        
     else:
         @property
         def cities(self):
@@ -30,3 +25,4 @@ class State(BaseModel, Base):
                 if value.state_id == self.id:
                     cities_in_state.append(value)
             return cities_in_state
+
